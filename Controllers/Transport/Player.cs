ae9f06ad090e1c632641c07c2c89ccd0c09e0b19
Ming Liu <mliu@compass.education>
2022-09-11 10:16:46 +1000

    add transport layer

---
+++ Controllers/Transport/Player.cs
@@ -0,0 +1,23 @@
+﻿
+namespace DepthChart01.Controllers.Transport
+{
+    public class Player
+    {
+        public string PlayerId { get; set; }
+
+        public string Name { get; set; }
+
+        public static Player FromModel(Models.Player player) => new Player()
+        {
+            PlayerId = player.PlayerId,
+            Name = player.Name,
+        };
+
+        public static Models.Player ToModel(Player player) => new Models.Player
+        {
+            PlayerId = player.PlayerId,
+            Name = player.Name
+        };
+    }
+
+}
